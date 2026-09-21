5af23b2b222352b6c9d89ca0d1c2ec077148be4b
Artur Truve <arturt@metropolia.fi>
2026-09-21 16:09:11 +0300

    Lisäsin pyydettyjä ominaisuuksia peliprojekti tehtävä 3. vaatimusten mukaan

--- peliprojekti/peliprojekti.py
+++ peliprojekti/peliprojekti.py
@@ -1,46 +1,74 @@
+print()
 nimi = input("Mikä sinun nimi on? ")
+print()
 ikä = input("Kuinka vanha olet? ")
+inventaario = []
+suunta = []
 
 game_state = True
+
+def mitä_lisätään():
+    esine = input("Lisää esine tai kirjoita 'lopeta': ")
+    while esine.lower() != "lopeta":
+        inventaario.append(esine)
+        esine = input("Seuraava esine jonka haluat lisätä: ")
+    return
+
+def inventaarion_sisältö():
+    for item in inventaario:
+        print(f"- {item}")
+    return
+
+def kartta():
+    suuntaan = input("Menitkö: Suoraan, Vasemmalle, Oikealle?\n")
+    while suuntaan.lower() != "lopeta":
+        suunta.append(suuntaan)
+        suuntaan = input("Mihin suuntaan seuraavaksi? Tai 'lopeta'\n")
+    return
+
+def lue_kartta():
+    print("Kartan mukaan olet edennyt seuraavasti: ")
+    for kohta in suunta:
+        print(f"{suunta.index(kohta)+1}. {kohta}")
+    return
 
 while game_state:
     if int(ikä) < 12:
         print("Olet alaikäinen. Ohjelma sammutetaan.")
         game_state = False
     else:
+        print()
         print(f"Tervetuloa {nimi}")
 
         while game_state:   # siirrytään valikkoon, jos käyttäjä on tarpeeksi vanha
+            print()
             print("Päävalikko: ")
-            print("1. Nimi")
-            print("2. Ikä")
-            print("3. Lopeta")
-            print("4. Vitsi")
+            print("1. Lisää esine inventaarioon")
+            print("2. Inventaarion sisältö")
+            print("3. Kartta")
+            print("4. Lue kartta")
+            print("5. Lopeta")
             komento = input("Anna komento: ")
 
-            if komento.lower() == "lopeta" or komento == "3":
-                print("Ohjelma sammutetaan.\n")
+            if komento.lower() == "lopeta" or komento == "5":
+                print()
+                print("Ohjelma sammutetaan.")
                 game_state = False
                 
-            elif komento.lower() == "nimi" or komento == "1":
-                print(f"Nimesi on {nimi}\n")
+            elif komento.lower() == "lisää esine inventaarioon" or komento == "1":
+                print()
+                mitä_lisätään()
 
-            elif komento.lower() == "ikä" or komento == "2":
-                print(f"Ikäsi on {ikä}\n")
+            elif komento.lower() == "inventaarion sisältö" or komento == "2":
+                print()
+                print("Inventaariossa on:")
+                inventaarion_sisältö()
 
-            elif komento.lower() == "vitsi" or komento == "4":
-                print("Miksi AMK-opiskelija haluaa ryhmätyökaverinsa arkunkantajaksi omissa hautajaisissaan?")
-                print("Jotta he voivat laskea hänet alas viimeisen kerran.\n")
+            elif komento.lower() == "kartta" or komento == "3":
+                print()
+                kartta()
 
-        
+            elif komento.lower() == "lue kartta" or komento == "4":
+                print()
+                lue_kartta()
 
-#######################
-# SEURAAVA VAIHE ALLA #
-#######################
-
-# Kehitä peliprojektia eteenpäin: Luo jokaiselle päävalikon toiminnolle (joita vähintään kolme) oma funktio,
-# joka suoritetaan, kun käyttäjä valitsee kyseisen toiminnon.
-
-# Yhden funktion pitää kysyä käyttäjältä asioita (esim. esine), jotka lisätään listamuuttujaan.
-# Toisen funktion pitää tulostaa listan sisältö käyttäjälle.
-# Muut toiminnot voi ideoida ja toteuttaa vapaasti.
